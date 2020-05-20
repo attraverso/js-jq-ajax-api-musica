2bfcedbbfec554fe2b876e0f0a0301f83fa654da
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-20 15:04:46 +0200

    bonus complete

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ $.ajax({
   'method': 'get',
   'success': function(data) {
     var cdArray = data.response;
+    console.log(cdArray);
     for (var i = 0; i < cdArray.length; i++) {
       var currentObject = cdArray[i];
       var html = template(currentObject);
@@ -18,5 +19,29 @@ $.ajax({
     alert('There was an issue with your request.')
   },
 })
+
+$('option[value=jazz]').click(function() {
+  $('.cd').hide();
+  $('.cd[data-genre=Jazz]').show();
+});
+
+$('option[value=pop]').click(function() {
+  $('.cd').hide();
+  $('.cd[data-genre=Pop]').show();
+});
+
+$('option[value=metal]').click(function() {
+  $('.cd').hide();
+  $('.cd[data-genre=Metal]').show();
+});
+
+$('option[value=rock]').click(function() {
+  $('.cd').hide();
+  $('.cd[data-genre=Rock]').show();
+});
+
+$('option[value=all]').click(function() {
+  $('.cd').show();
+});
 
 });/*DNT - closing doc.ready*/
